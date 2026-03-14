707adeb8db6daf6601526c8d6442e551b2cb2559
Abhimanyu1221 <165375245+Abhimanyu1221@users.noreply.github.com>
2026-03-14 19:35:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -2,6 +2,7 @@ class Solution:
     def addDigits(self, num: int) -> int:
         if num==0:
             return 0
-        val  = num-((9)*((num-1)//(9)))
+        # val  = num-((9)*((num-1)//(9)))
+        val = 1 + ((num-1)%9)
         print(val)
         return val
